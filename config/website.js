bc542a040212e0c847c237ceb7c74c83186b694f
yanochoa <yanochoa@email.arizona.edu>
2019-02-12 00:40:36 -0700

    edit to the Helmet and removed resume from about page because google docs messes up the styling of the documenton mobile

--- config/website.js
+++ config/website.js
@@ -1,14 +1,14 @@
 module.exports = {
   pathPrefix: "/",
   siteTitle: "Yan Ochoa",
-  siteTitleAlt: "Yan Ochoa - Software Engineer",
+  siteTitleAlt: "Yan Ochoa - Software Developer",
   siteHeadline: "Looking to be 1% less worse every day",
   siteTitleShort: "Yan", // short_name for manifest
-  siteUrl: "https://emma.lekoarts.de", // Domain of your site. No trailing slash!
+  siteUrl: "https://yanochoa.com", // Domain of your site. No trailing slash!
   siteLanguage: "en", // Language Tag on <html> element
   siteLogo: "/logo.png", // Used for SEO and manifest
   siteDescription:
-    "Showcasing projects and interests of me Yan Ochoa, a Software Engineer",
+    "Showcasing projects and interests of me Yan Ochoa, a Software Developer",
   author: "Yan Ochoa",
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
